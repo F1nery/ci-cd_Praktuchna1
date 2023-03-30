2fe9d3a2b59e54e147955c9f546b0576c3036f82
Roman <f1nery@knu.ua>
2023-03-30 11:13:53 +0300

    added func sort_txt_data_by_population

--- script.py
+++ script.py
@@ -7,3 +7,6 @@ def sort_txt_data_by_area(data):
     sorted_data = sorted(data, key=lambda x: int(x[1]))
     return sorted_data
 
+def sort_txt_data_by_population(data):
+    sorted_data = sorted(data, key=lambda x: int(x[2]))
+    return sorted_data
