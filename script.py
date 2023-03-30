4c3cde5a4c76fd0e3f0a62a16adc214b684db502
Roman <f1nery@knu.ua>
2023-03-30 11:11:57 +0300

    added func sort_txt_data_by_area

--- script.py
+++ script.py
@@ -3,3 +3,7 @@ def read_txt_data(filename):
         data = [line.strip().split(',') for line in f]
     return data
 
+def sort_txt_data_by_area(data):
+    sorted_data = sorted(data, key=lambda x: int(x[1]))
+    return sorted_data
+
